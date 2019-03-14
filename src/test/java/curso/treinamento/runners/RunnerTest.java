bcc529f2788d7a2a6d603d08245a8e60f1f7f7e2
Romullo Santos <romullo.os@gmail.com>
2019-03-14 09:10:40 -0300

    Estrutura de Projeto

---
+++ src/test/java/curso/treinamento/runners/RunnerTest.java
@@ -0,0 +1,14 @@
+package curso.treinamento.runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	features = {"src/test/resources/features/"},
+	glue = {"curso.treinamento.setps","curso.treimnamento.setup"},
+	tags = {})
+
+public class RunnerTest{}
---
+++ target/test-classes/features/login.feature
@@ -0,0 +1,8 @@
+# language: pt
+
+Funcionalidade: login
+
+Cenário: Realizar login
+Dado: que eu esteja na tela de login
+Quando: faço login com usuário "mercury" e senha "mercury"
+Então: sou autenticado com sucesso
